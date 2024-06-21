936b4775f7220823065c477767590982aa16f6fe
Mariklimova <mariku22@yandex.ru>
2024-06-22 01:04:42 +0300

    added tasks 38-47

--- 31/app.js
+++ 31/app.js
@@ -17,6 +17,7 @@ const checkMaxCount = (arr) => {
             title: arr[i],
             count: 0
         }
+
         for (let a = 0; a < arr.length; a++) {
             if (arr[a] === tampalaruObj.title) {
                 tampalaruObj.count += 1
@@ -37,15 +38,45 @@ console.log(checkMaxCount(arr));
 
 
 // 2
-// const result_2 =()=>{
+const arr_4 = ['dtf', 'dtf', 'gj', 'rfdyuoijp', 'gj', 'jhygu', 'gresehjryyuuou', 'gj'];
+const result_4 = () => {
+
+    const obj = {};
+
+    for (let i = 0; i < arr_4.length; i++) {
+        obj[arr_4[i]] ? obj[arr_4[i]] += 1 : obj[arr_4[i]] = 1;
+    }
+
+    const newArr = [];
+
+    for (const key in obj) {
+        newArr.push(obj[key])
+    }
+
+    for (const key in obj) {
+        if (obj[key] === Math.max(...newArr)) return key
+    }
+}
+console.log(result_4());
+
+
+
+
+
+// 2
+// const result_2 = () => {
 
 //     const countEl = {};
 
 //     for (const el of arr) {
 //         countEl[el] = countEl[el] ? countEl[el] + 1 : 1;
 //     }
-    
-//     return Object.keys(countEl).filter((elem) => countEl[elem] > 1);
+
+//     const max = Math.max(...Object.values(countEl))
+//     if (Object.values(countEl) == max) {
+//         return Object.keys(countEl)
+//     }
+
 // }
 // console.log(result_2());
 
@@ -55,15 +86,12 @@ console.log(checkMaxCount(arr));
 // 3
 // const res = arr.reduce((obj, key) => {
 //     if (obj.hasOwnProperty(key)) {
-//       obj[key] += 1;
+//         obj[key] += 1;
 //     } else {
-//       obj[key] = 1;
+//         obj[key] = 1;
 //     }
 //     return obj;
-//   },{})
-//   console.log(res);
-  
-
-
+// }, {})
+// console.log(res);
 
 
