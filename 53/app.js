82ad7fb662730427b0cb071d8078a697644f91c2
Mariklimova <mariku22@yandex.ru>
2024-06-24 04:12:28 +0300

    added tasks 48-52

---
+++ 53/app.js
@@ -0,0 +1,20 @@
+// 53. Реализуйте функцию, которая принимает строку в формате JSON и возвращает объект, 
+// соответствующий этой строке. Не используйте встроенные методы JSON.parse
+
+
+const jsonStr = '{“bookname" : ”geekforgeeks“, ”author“ : ”bharat“, "ReleaseYear” : “2001”}';
+
+// 1
+const result = () => {
+
+
+}
+console.log(result());
+
+
+
+// 2
+const result_2 = () =>
+console.log(result_2());
+
+
--- 9/app.js
+++ 9/app.js
@@ -2,7 +2,7 @@
 // простым, и false в противном случае.
 
 
-const num = 7;
+const num = 8;
 let check = 0;
 
 // 1
@@ -16,6 +16,18 @@ console.log(result());
 
 
 // 2
+let flag = true;
+const result_2 = () => {
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) flag = false;
+    }
+    return flag ? true : false;
+
+};
+console.log(result_2());
+
+
+
 // const result_2 = (num) => {
 //     if (num === 1) return;
 //     if (num % result_2(num - 1) === 0) {
